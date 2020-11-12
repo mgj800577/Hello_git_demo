c756309187ea866c8b587bf309309a1167b1706a
Administrator_mgj <mgj800577@163.com>
2020-11-12 20:47:04 +0800

    版本2

--- HelloGit/src/main/java/com/mgj/HelloGit.java
+++ HelloGit/src/main/java/com/mgj/HelloGit.java
@@ -3,5 +3,6 @@ package com.mgj;
 public class HelloGit {
     public static void main(String[] args) {
         System.out.println("Hello Git!!");
+        System.out.println("Hello 版本2");
     }
 }
