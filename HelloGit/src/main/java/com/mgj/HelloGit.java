2e672e473d739ef2f3401aeaeeae2e533e52c8f0
Administrator_mgj <mgj800577@163.com>
2020-11-12 20:57:25 +0800

    创建分支

--- HelloGit/src/main/java/com/mgj/HelloGit.java
+++ HelloGit/src/main/java/com/mgj/HelloGit.java
@@ -4,5 +4,6 @@ public class HelloGit {
     public static void main(String[] args) {
         System.out.println("Hello Git!!");
         System.out.println("Hello 版本2");
+        System.out.println("Hello 创建分支");
     }
 }
