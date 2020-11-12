97fbe7c9a3b2225e5bd2745537e3170e37c4ddda
Administrator_mgj <mgj800577@163.com>
2020-11-12 21:28:29 +0800

    主干更新

--- HelloGit/src/main/java/com/mgj/HelloGit.java
+++ HelloGit/src/main/java/com/mgj/HelloGit.java
@@ -7,5 +7,6 @@ public class HelloGit {
         System.out.println("Hello 创建分支");
         System.out.println("Hello 主干添加");
         System.out.println("Hello 分支添加");
+        System.out.println("Hello 主干更新");
     }
 }
